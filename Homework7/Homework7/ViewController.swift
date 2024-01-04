455abde6aa487215cb3e3fd7c3f30d04f5d78538
LaptevaKate <ekaterina213@yandex.ru>
2024-01-04 19:36:03 +0300

    init homework7

---
+++ Homework7/Homework7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework7
+//
+//  Created by Екатерина Лаптева on 4.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
